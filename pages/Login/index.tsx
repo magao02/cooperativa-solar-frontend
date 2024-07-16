862eaffcdc2cc68fa8be838372ef810250d961e3
ricsjs <ricfilho0007@gmail.com>
2024-07-16 09:49:32 -0300

    feat: create login scren

--- pages/Login/index.tsx
+++ pages/Login/index.tsx
@@ -1,23 +1,54 @@
-import {Button} from "@nextui-org/button";
-import {Input} from "@nextui-org/react";
-import { useState } from 'react';
+import { useState } from "react";
+import { Button } from "@nextui-org/react";
+import { Input, Spacer, Card } from "@nextui-org/react";
 
 export default function Login() {
-  const [username, setUsername] = useState('')
-  const doLogin =  ()=>{
-    
-  }
-  return(
-    <div>
-      <h1>Login</h1>
-      <form>
-        <Input type="text" placeholder="Username"/>
-        <Input type="password" placeholder="Password"/>
-      </form>
-      <Button onClick={doLogin}>Login</Button>
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const doLogin = () => {
+    if (!username || !password) {
+      setError("Both fields are required.");
+    } else {
+      setError("");
+      console.log("Logging in with", { username, password });
+    }
+  };
+
+  return (
+    <div style={{display: "flex", alignItems: "center", justifyContent: "center", minHeight: "100vh"}}>
+      <Card style={{ width: "400px", padding: 20}}>
+      <Spacer y={4} />
+      <h1 style={{ textAlign:"center", marginBottom: "10" }}>Faça login</h1>
+      <Spacer y={4} />
+        <form onSubmit={(e) => e.preventDefault()}>
+        
+          <Input
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Spacer y={1} />
+          <Input
+            fullWidth
+            color="primary"
+            size="lg"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Spacer y={1} />
+          <Spacer y={1.5} />
+          <Button onClick={doLogin} style={{ width: "100%" }}>
+            Login
+          </Button>
+        </form>
+      </Card>
     </div>
-    
-  )
-  
-  
+  );
 }
